809821911f667e20f5cd528db789abb814ae7061
Clay Ewing <c.ewing@miami.edu>
2021-12-15 13:42:52 -0600

    clean up css

--- index.php
+++ index.php
@@ -19,67 +19,67 @@
     <section class="section">
       <div class="container">
         <h2>Call Dibs on Your REF!</h2>
+    	<div class="columns">
+    		<div class="column is-one-fifth">
+		    <?php $dir = new DirectoryIterator(dirname(__FILE__).'/items/'); ?>
+		    <?php foreach($dir as $fileinfo): ?>
+		    	<?php if(!$fileinfo->isDot()):?>
+						<a class="modal-button" href="#" data-target="#modal-<?php echo $fileinfo->getBasename('.png');?>">
+							<img src="items/<?php echo $fileinfo->getFilename();?>" alt="Image" class="image is-128x128"/>
+						</a>
+
+						<div class="modal" id="modal-<?php echo $fileinfo->getBasename('.png');?>">
+						  <div class="modal-background"></div>
+						  <div class="modal-content">
+							<div class="box">
+							    <form>
+
+							      <article class="media">
+							        <div class="media-left">
+							          <figure class="image is-64x64">
+							            <img src="items/<?php echo $fileinfo->getFilename();?>" alt="Image"/>
+							          </figure>
+							        </div>
+							        <div class="media-content">
+							          	<div class="content">
+							            	<div class="field">
+							            		<label class="label">Name</label>
+							            		<div class="control">
+							            			<input class="input" type="text" placeholder="Your Name" name="name">
+							            		</div>
+							            	</div>
+							            	<input type="hidden" name="filename" value="<?php echo $fileinfo->getFilename();?>">
+							          	</div>
+							          
+							          <nav class="level-right">
+							              <a class="level-item button is-primary" aria-label="claim">
+							              	Dibs!
+							                </a>
+							          </nav>
+							        </div>
+							      </article>
+							    </form>
+							</div>
+						  </div>
+						  <button class="modal-close is-large" aria-label="close"></button>
+						</div>
+		    	<?php endif;?>
+
+		    <?php endforeach;?>
+		</div>
+		</div>
       </div>
-    
-    <?php $dir = new DirectoryIterator(dirname(__FILE__).'/items/'); ?>
-    <?php foreach($dir as $fileinfo): ?>
-    	<?php if(!$fileinfo->isDot()):?>
-				<a class="modal-button" href="#" data-target="#modal-<?php echo $fileinfo->getBasename('.png');?>"><img src="items/<?php echo $fileinfo->getFilename();?>" alt="Image" class="image is-128x128"/></a>
-
-				<div class="modal" id="#modal-<?php echo $fileinfo->getBasename('.png');?>">
-				  <div class="modal-background"></div>
-				  <div class="modal-content">
-					<div class="box">
-					    <form>
-
-					      <article class="media">
-					        <div class="media-left">
-					          <figure class="image is-64x64">
-					            <img src="items/<?php echo $fileinfo->getFilename();?>" alt="Image"/>
-					          </figure>
-					        </div>
-					        <div class="media-content">
-					          	<div class="content">
-					            	<div class="field">
-					            		<label class="label">Name</label>
-					            		<div class="control">
-					            			<input type="text" placeholder="Your Name" name="name">
-					            		</div>
-					            	</div>
-					            	<input type="hidden" name="filename" value="<?php echo $fileinfo->getFilename();?>">
-					          	</div>
-					          
-					          <nav class="level is-mobile">
-					            <div class="level-right">
-					              <a class="level-item" aria-label="claim">
-					              	Dibs!
-					                </a>
-					            </div>
-					          </nav>
-					        </div>
-					      </article>
-					    </form>
-					</div>
-				  </div>
-				  <button class="modal-close is-large" aria-label="close"></button>
-				</div>
-    	<?php endif;?>
-
-    <?php endforeach;?>
     </section>
 
 <script>
 document.querySelectorAll('.modal-button').forEach(function(el) {
   el.addEventListener('click', function() {
     var target = document.querySelector(el.getAttribute('data-target'));
-    console.log(target);
-//    target.classList.add('is-active');
-    
+    target.classList.add('is-active');    
     target.querySelector('.modal-close').addEventListener('click',   function() {
         target.classList.remove('is-active');
      });
   });
-})
-;</script>
+});</script>
 </body>
 </html>
