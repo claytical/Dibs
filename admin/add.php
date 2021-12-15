802c2a81a38eb8cb8a57f207803521648120a3be
Clay Ewing <c.ewing@miami.edu>
2021-12-15 14:33:58 -0600

    check for existing files before adding to database

--- admin/add.php
+++ admin/add.php
@@ -7,18 +7,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Rich Entitled Fucks</title>
-    <link rel="stylesheet" href="css/main.css">
+    <title>Dibs</title>
+    <link rel="stylesheet" href="../css/main.css">
   </head>
   <body>
     <section class="hero is-medium has-background-success">
         <div class="container is-max-desktop has-text-centered">
-              <img src="ref_logo.png" width="25%"/>
+        <h2>Adding Items to Claim Dibs On</h2>
         </div>
     </section>
     <section class="section">
       <div class="container">
-        <h2>Adding Items to Claim Dibs On...</h2>
     			<div class="columns">
 		    	<?php $dir = new DirectoryIterator(dirname(__FILE__).'/../items/'); ?>
 		    	<?php foreach($dir as $fileinfo): ?>
@@ -28,10 +27,18 @@
 								<img src="../items/<?php echo $fileinfo->getFilename();?>" alt="Image" class="image is-128x128"/>
 								
 								<?php
+									//CHECK IF ITEM ALREADY IN DATABASe
+									$count = $database->count("items"[
+										"name" => $fileInfo->getFilename()]);
+
+
 									//ADD ITEMS TO DATABASE
-									$database->insert("items", [
-										"filename" => $fileinfo->getFilename()
-									]);
+									if($count > 0) {
+										$database->insert("items", [
+											"filename" => $fileinfo->getFilename()
+										]);
+										
+									}
 								?>
 							
 							</div>
